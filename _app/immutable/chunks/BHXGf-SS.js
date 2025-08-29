b9f804b2e8007acbe679c833913be1b63820ca54
Jacob K <jacob.klemick@outlook.com>
2025-08-30 02:16:15 +1200

    Test Commit

---
+++ _app/immutable/chunks/BHXGf-SS.js
@@ -0,0 +1,78 @@
+import"./DsnmJJEf.js";import{i as z}from"./Dt0WG65Q.js";import{P as f,F as j,Q as G,D as l,R as g,e as B,M as F,L as C,G as v,H as m,ay as D,a2 as y,A as k,B as _,C as M,aE as N,S as d,aD as H}from"./FobJdX3h.js";import{p as h,s as P,r as I,i as S}from"./UelL07CU.js";import{e as w,s as J}from"./_OYxjPpy.js";import{s as A,a as E,I as L}from"./CfLVxiYC.js";import{c as K}from"./DMEwKkOG.js";var Q=j('<span><a target="_blank" rel="noopener noreferrer"> </a></span>');function R(r,t){f(t,!1);let s=h(t,"link",8),n=h(t,"text",8),o=D();z();var i=Q(),e=v(i),a=v(e,!0);m(e),m(i),G(p=>{A(e,1,`text-link ${C(o)?"hover":"normal"}`,"svelte-189pw89"),E(e,"href",s()),J(a,p)},[()=>(F(n()),B(()=>n().trim()))]),w("mouseenter",e,()=>y(o,!0)),w("mouseleave",e,()=>y(o,!1)),l(r,i),g()}function T(r,t){f(t,!0);/**
+ * @license @lucide/svelte v0.528.0 - ISC
+ *
+ * ISC License
+ *
+ * Copyright (c) for portions of Lucide are held by Cole Bemis 2013-2022 as part of Feather (MIT). All other copyright (c) for Lucide are held by Lucide Contributors 2022.
+ *
+ * Permission to use, copy, modify, and/or distribute this software for any
+ * purpose with or without fee is hereby granted, provided that the above
+ * copyright notice and this permission notice appear in all copies.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+ * WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+ * MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+ * ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+ * WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+ * ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+ * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+ *
+ */let s=I(t,["$$slots","$$events","$$legacy"]);const n=[["path",{d:"M15 22v-4a4.8 4.8 0 0 0-1-3.5c3 0 6-2 6-5.5.08-1.25-.27-2.48-1-3.5.28-1.15.28-2.35 0-3.5 0 0-1 0-3 1.5-2.64-.5-5.36-.5-8 0C6 2 5 2 5 2c-.3 1.15-.3 2.35 0 3.5A5.403 5.403 0 0 0 4 9c0 3.5 3 5.5 6 5.5-.39.49-.68 1.05-.85 1.65-.17.6-.22 1.23-.15 1.85v4"}],["path",{d:"M9 18c-4.51 2-5-2-7-2"}]];L(r,P({name:"github"},()=>s,{get iconNode(){return n},children:(o,i)=>{var e=k(),a=_(e);M(a,()=>t.children??N),l(o,e)},$$slots:{default:!0}})),g()}function Z(r,t){f(t,!0);/**
+ * @license @lucide/svelte v0.528.0 - ISC
+ *
+ * ISC License
+ *
+ * Copyright (c) for portions of Lucide are held by Cole Bemis 2013-2022 as part of Feather (MIT). All other copyright (c) for Lucide are held by Lucide Contributors 2022.
+ *
+ * Permission to use, copy, modify, and/or distribute this software for any
+ * purpose with or without fee is hereby granted, provided that the above
+ * copyright notice and this permission notice appear in all copies.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+ * WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+ * MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+ * ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+ * WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+ * ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+ * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+ *
+ */let s=I(t,["$$slots","$$events","$$legacy"]);const n=[["circle",{cx:"12",cy:"12",r:"10"}],["path",{d:"M12 2a14.5 14.5 0 0 0 0 20 14.5 14.5 0 0 0 0-20"}],["path",{d:"M2 12h20"}]];L(r,P({name:"globe"},()=>s,{get iconNode(){return n},children:(o,i)=>{var e=k(),a=_(e);M(a,()=>t.children??N),l(o,e)},$$slots:{default:!0}})),g()}function q(r,t){f(t,!0);/**
+ * @license @lucide/svelte v0.528.0 - ISC
+ *
+ * ISC License
+ *
+ * Copyright (c) for portions of Lucide are held by Cole Bemis 2013-2022 as part of Feather (MIT). All other copyright (c) for Lucide are held by Lucide Contributors 2022.
+ *
+ * Permission to use, copy, modify, and/or distribute this software for any
+ * purpose with or without fee is hereby granted, provided that the above
+ * copyright notice and this permission notice appear in all copies.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+ * WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+ * MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+ * ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+ * WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+ * ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+ * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+ *
+ */let s=I(t,["$$slots","$$events","$$legacy"]);const n=[["path",{d:"M16 8a6 6 0 0 1 6 6v7h-4v-7a2 2 0 0 0-2-2 2 2 0 0 0-2 2v7h-4v-7a6 6 0 0 1 6-6z"}],["rect",{width:"4",height:"12",x:"2",y:"9"}],["circle",{cx:"4",cy:"4",r:"2"}]];L(r,P({name:"linkedin"},()=>s,{get iconNode(){return n},children:(o,i)=>{var e=k(),a=_(e);M(a,()=>t.children??N),l(o,e)},$$slots:{default:!0}})),g()}function O(r,t){f(t,!0);/**
+ * @license @lucide/svelte v0.528.0 - ISC
+ *
+ * ISC License
+ *
+ * Copyright (c) for portions of Lucide are held by Cole Bemis 2013-2022 as part of Feather (MIT). All other copyright (c) for Lucide are held by Lucide Contributors 2022.
+ *
+ * Permission to use, copy, modify, and/or distribute this software for any
+ * purpose with or without fee is hereby granted, provided that the above
+ * copyright notice and this permission notice appear in all copies.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+ * WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+ * MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+ * ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+ * WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+ * ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+ * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+ *
+ */let s=I(t,["$$slots","$$events","$$legacy"]);const n=[["path",{d:"m22 7-8.991 5.727a2 2 0 0 1-2.009 0L2 7"}],["rect",{x:"2",y:"4",width:"20",height:"16",rx:"2"}]];L(r,P({name:"mail"},()=>s,{get iconNode(){return n},children:(o,i)=>{var e=k(),a=_(e);M(a,()=>t.children??N),l(o,e)},$$slots:{default:!0}})),g()}var V=j('<a target="_blank" rel="noopener noreferrer"><!></a>');function x(r,t){let s=h(t,"icon",8),n=h(t,"link",8);const i={Globe:Z,Github:T,Mail:O,Linkedin:q}[s()];let e=D(!1);var a=V(),p=v(a);{var b=c=>{var $=k(),u=_($);K(u,()=>i,(U,W)=>{W(U,{})}),l(c,$)};S(p,c=>{i&&c(b)})}m(a),G(()=>{A(a,1,`icon-link ${C(e)?"hover":"normal"}`,"svelte-nr9rvx"),E(a,"href",n())}),w("mouseenter",a,()=>y(e,!0)),w("mouseleave",a,()=>y(e,!1)),l(r,a)}var X=j(`<div class="profile-card svelte-ulo2c"><img id="portrait" src="assets/images/Port.webp" alt="Me" class="svelte-ulo2c"/> <h1>Jacob Klemick</h1> <p class="svelte-ulo2c">Software Developer | Chirstchurch, NZ</p> <p class="svelte-ulo2c">Experience in cross-platform mobile app development, two years working with
+    the Unity game engine, and recently <!>. Skilled in data analysis and architecture, I'm also refreshing and learning new web development skills.</p> <p class="svelte-ulo2c">In my spare time, I like to do 3D modeling and product design.</p> <span class="icon-link-parent svelte-ulo2c"><!> <!> <!> <!></span></div>`);function se(r,t){let s=h(t,"includeWebsite",8,!1);var n=X(),o=d(v(n),6),i=d(v(o));R(i,{link:"/projects/gnss-simulation",text:"working on a large-scale Unity project"}),H(),m(o);var e=d(o,4),a=v(e);{var p=u=>{x(u,{icon:"Globe",link:"/"})};S(a,u=>{s()==!0&&u(p)})}var b=d(a,2);x(b,{icon:"Mail",link:"mailto:jacob.klemick@outlook.com"});var c=d(b,2);x(c,{icon:"Linkedin",link:"https://www.linkedin.com/in/jacob-klemick"});var $=d(c,2);x($,{icon:"Github",link:"https://github.com/jkk0035-bunny3dnt45"}),m(e),m(n),l(r,n)}export{se as P};
